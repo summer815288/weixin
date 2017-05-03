745ead2fa2c006a8167260294cf75c4d118408e7
buchenhui <2543567472@qq.com>
2017-05-03 11:18:44 +0800

    bu

--- weixin/application/database.php
+++ weixin/application/database.php
@@ -19,7 +19,7 @@ return [
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'bch1996.8.18',
+    'password'        => 'root',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
